cd703e3b67586e6a3d2b59b4bbd250520d956223
houssemlachtar <lachtarhoussem4@gmail.com>
2022-06-21 11:15:27 +0100

    Incredibox Feat m&m's And JessieJ

---
+++ lang/en.js
@@ -0,0 +1,48 @@
+var translateObj = {
+    code: 'EN',
+    name: 'English',
+    trad: {
+        bt: {
+            all: 'All',
+            yes: 'Yes',
+            ok: 'Ok',
+            sure: 'Sure',
+            no: 'No',
+            nope: 'Nope',
+            later: 'Later',
+            update: 'Update',
+            retry: 'Retry',
+            replay: 'Replay',
+            create: 'Create',
+            share: 'Share',
+            save: 'Save',
+            info: 'Info',
+            cancel: 'Cancel',
+            more: 'More',
+            understand: 'I understand'
+        },
+        txt: {
+            replayTitle: "Replay mode",
+            replayText: "Pump up the volume and enjoy your beat!",
+            randomTitle: "Random mode",
+            randomText: "Let the beat play itself, pump up the volume and enjoy!",
+        },
+        pop: {
+            dragDropTitle: "Drag 'n drop",
+            dragDropText: "Drag and drop an icon on<br>a character to start recording",
+            toShortTitle: "Too short",
+            toShortText: "To record your beat you have to record<br>at least 30 seconds",
+            bonusPlayingTitle: "Bonus playing",
+            bonusPlayingText: "You can't start recording<br>while bonus is playing",
+            recOkTitle: "Beat finished",
+            replayOkTitle: "Create your own beat",
+            replayOkText: "Do you want to create your own beat?",
+            formTitle: "Save your beat",
+            shareTitle: "Beat saved",
+            pauseTitle: "Game paused",
+            pauseText: "Hey dude, where did you go?<br>Ready to beat again?",
+            sharingBugTitle: "Sorry buddy",
+            sharingBugText: "We lost connection and can't save<br>your beat right now. Please try again.",
+        }
+    }
+};
